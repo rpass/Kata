5c1a1ca6d77f6038c5a9b487a5fbddaeee90028c
rpass <robertmpassmore@gmail.com>
2016-02-09 18:38:42 +0200

    Started 2nd attempt at String calculator TDD code kata using javascript with the jasmine test framework.

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -3,18 +3,61 @@
  */
 describe('Calculator Test Suite', function () {
     var calc;
+    var sum;
 
     beforeEach(function () {
-       calc = new Calculator();
+        calc = new Calculator();
+        sum = 0;
     });
 
     it('should have a method called Add', function () {
-       expect(calc.add()).toBeDefined();
+        expect(calc.add).toBeDefined();
     });
 
     it('should take an empty string and return 0', function () {
         expect(calc.add('')).toBe(0);
     });
 
+    it('should take a single integer input and return it', function () {
+        sum = calc.add('1');
+        expect(sum).toEqual(1);
+    });
 
+    it('should take two comma delimited integers and return their sum', function () {
+        sum = calc.add('1,2');
+        expect(sum).toEqual(3);
+    });
+
+    it('should take more than two comma delimited integers and return their sum',
+        function () {
+            sum = calc.add('1,2,3');
+            expect(sum).toEqual(6);
+        });
+
+    it('should take two new line delimited integers and return their sum', function () {
+        sum = calc.add('1\n2');
+        expect(sum).toEqual(3);
+    });
+
+    it('should take more than three comma delimited integers and return their sum',
+        function () {
+            sum = calc.add('1,2,3,4');
+            expect(sum).toEqual(10);
+        });
+
+    it('should take more than three new line delimited integers and return their sum',
+        function () {
+            sum = calc.add('1\n2\n3\n4');
+            expect(sum).toEqual(10);
+        });
+
+    it('should take more than three \'new line\' or comma delimited integers and return their sum',
+        function () {
+            sum = calc.add('1\n2,3\n4');
+            expect(sum).toEqual(10);
+        });
+
+    describe('Extract Delimiter function test suite', function () {
+
+    });
 });
--- src/Calculator.js
+++ src/Calculator.js
@@ -3,7 +3,42 @@
  */
 var Calculator;
 Calculator = function () {
+
+    var standardDelimiters = /[,]|[\n]/;
+
+    var extractDelimiters = function(input){
+        var customDelimiter = input.substr(0,input.indexOf('\n')).match(/\/\/(.*)\n/);
+        var delimiter = standardDelimiters;
+        if(customDelimiter.length > 1){
+            delimiter = customDelimiter;
+        } else {
+            delimiter = standardDelimiters;
+        }
+        return delimiter;
+    };
+
+    var extractOperands = function (input, delimeterRE) {
+        var operands = [];
+        var inputs = input.split(delimeterRE);
+        inputs.forEach(function (x) {
+            operands.push(parseInt(x));
+        });
+        return operands;
+    };
+
+    var reduceInputsToSum = function(input){
+        return extractOperands(input, standardDelimiters).reduce(function (prev, curr) {
+            return prev + curr;
+        }, 0);
+    };
+
     this.add = function (input) {
-        return input === '' ? 0 : null;
+        var sum = 0;
+        if (input !== '') {
+            sum = reduceInputsToSum(input);
+            return sum;
+        } else {
+            return 0;
+        }
     };
 };
